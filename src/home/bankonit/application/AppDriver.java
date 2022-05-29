9082d43c8ae4476d2135ad193e0dd002120e18dc
Night-Cheza <leila.kassi@outlook.com>
2022-05-28 23:01:55 -0600

    AppDriver class code is cleaned

---
+++ src/home/bankonit/application/AppDriver.java
@@ -0,0 +1,24 @@
+package home.bankonit.application;
+
+import home.bankonit.gui.*;
+
+/**
+ * Application driver
+ * @author Nick Hamnett
+ * @version August 3, 2021
+ */
+public class AppDriver {
+
+	/**
+	 * Entry point for program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+ 		LoginWindow login = new LoginWindow();
+		login.setVisible(true);
+		
+	}
+
+}
